c4151be3344e6390fc263594b6d49a9957dd139b
mjshariati98 <mjshariati98@gmail.com>
2019-08-25 01:47:44 +0430

    test

---
+++ build.sbt
@@ -0,0 +1,19 @@
+import Dependencies._
+
+lazy val root = (project in file(".")).
+  settings(
+    inThisBuild(List(
+      organization := "org.ermilov",
+      scalaVersion := "2.11.12",
+      version      := "1.0.0-SNAPSHOT"
+    )),
+    name := "SparkWriteApplication",
+    libraryDependencies += scalaTest % Test,
+    mainClass in assembly := Some("org.ermilov.SparkWriteApplication"),
+    test in assembly := {}
+  )
+
+assemblyMergeStrategy in assembly := {
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+}
---
+++ src/main/scala/org/ermilov/SparkWriteApplication.scala
@@ -0,0 +1,21 @@
+package org.ermilov
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object SparkWriteApplication extends App {
+  val config = new SparkConf().setMaster("spark://localhost:7077").setAppName("SparkWriteApplication")
+    .set("spark.driver.memory", "500m")
+    .set("spark.executor.memory", "500m")
+    .set("spark.executor.memoryOverhead", "100m")
+    .set("spark.executor.cores", "4")
+    .set("spark.cores.max", "4")
+    .set("spark.executor.instances", "1")
+
+
+  val sc = new SparkContext(config)
+
+  val numbersRdd = sc.parallelize((1 to 80000).toList)
+  print(numbersRdd.count)
+    numbersRdd.saveAsTextFile("hdfs://localhost:8020/tmp/nsadsdasad-umbers-as-text-4564")
+}
+
