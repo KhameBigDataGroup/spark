8b458c46c3c6196af71d9153ec5ddb03bd3231a2
mjshariati98 <mjshariati98@gmail.com>
2019-08-25 04:33:40 +0430

    change group_id

--- src/main/scala/org/ermilov/KafkaConsumerFactory.scala
+++ src/main/scala/org/ermilov/KafkaConsumerFactory.scala
@@ -14,7 +14,7 @@ object KafkaConsumerFactory extends Serializable {
     "bootstrap.servers" -> brokers,
     "key.deserializer" -> classOf[StringDeserializer],
     "value.deserializer" -> classOf[StringDeserializer],
-    "group.id" -> "gid",
+    "group.id" -> "gid2",
     "auto.offset.reset" -> "earliest",
     "enable.auto.commit" -> (false: java.lang.Boolean))
 
